3281df0a0702f63367634c892f8f3d9e16f8140c
Musanna <musannaal@gmail.com>
2023-08-06 00:16:10 +0600

    another commit

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.messagingSenderId,
-  appId: process.env.APP_ID
+  apiKey: `${process.env.API_KEY}`,
+  authDomain: `${process.env.AUTH_DOMAIN}`,
+  projectId: `${process.env.PROJECT_ID}`,
+  storageBucket: `${process.env.STORAGE_BUCKET}`,
+  messagingSenderId: `${process.env.messagingSenderId}`,
+  appId: `${process.env.APP_ID}`
 };
 
 // Initialize Firebase
