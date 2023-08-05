6cabee031fc2a74ed19fd941b0dae70c55b5c46e
Musanna <musannaal@gmail.com>
2023-08-06 00:01:00 +0600

    add init.js for deployment

---
+++ src/firebase/init.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.APP_ID
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
+//init firestore service
+const db = getFirestore();
+export default db;
